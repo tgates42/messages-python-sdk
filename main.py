e0c082a21fae9359c34180be8fbbdc2959233c9b
Tim Gates <tgates@iress.com.au>
2018-03-22 15:54:45 +1200

    Fix up format

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import message_media_messages.message_media_messages_client
 import getpass
 client = message_media_messages.message_media_messages_client.MessageMediaMessagesClient(
-     auth_user_name=getpass.getpass('API Key'),
-     auth_password=getpass.getpass('API Pass'),
+     auth_user_name=getpass.getpass('API Key: '),
+     auth_password=getpass.getpass('API Pass: '),
 )
 client.messages.create_send_messages(
         {
             "messages": [
                 {
                     "content": "I want a Nintendo Switch!",
-                     "destination_number": getpass.getpass('Phone'),
+                     "destination_number": getpass.getpass('Phone: '),
                 }
             ]
         }
